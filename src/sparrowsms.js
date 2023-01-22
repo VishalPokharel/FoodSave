80df0518c7280a6442394f1288f433d2af1c9052
Itshyphen <ranjugc69@gmail.com>
2023-01-22 14:53:23 +0545

    sms

--- src/sparrowsms.js
+++ src/sparrowsms.js
@@ -1,8 +1,7 @@
 const token = 'v2_HL2H2LJkZp2o55xbko7cOPU8a34.D2OU';
 const from = 'TheAlert';
-const to = '9866009221,9884033669';
 
-const sendsms=(text)=>{
+export const sendsms=(text,to)=>{
     fetch('http://api.sparrowsms.com/v2/sms/', {
     method: 'POST',
     headers: {
